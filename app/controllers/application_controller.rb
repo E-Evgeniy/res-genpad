6f68a811d2b5b1072f0105294631744cc4d2285c
Evg <evgxmn2@gmail.com>
2022-09-12 14:57:28 +0300

    test controller code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Test comment
 class ApplicationController < ActionController::Base
 end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# Test comment
+
 class ResultsController < ApplicationController
 end
