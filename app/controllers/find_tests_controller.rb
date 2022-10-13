4d7c621d68f1c20f53f621a5b2c85241b6ba73e4
Evg <evgxmn2@gmail.com>
2022-10-13 17:00:58 +0300

    work1

--- app/controllers/find_tests_controller.rb
+++ app/controllers/find_tests_controller.rb
@@ -4,7 +4,7 @@ class FindTestsController < ApplicationController
 
   def result
     hash_data = find_hash_data(params[:data_begin], params[:data_end])
-    return if hash_data['data_begin'].nil?
+    return if hash_data.nil?
     @tests = Test.find_by_sql(["SELECT * from tests where testing_date between ? and ?",
          hash_data['data_begin'], hash_data['data_end']])
   end
@@ -12,6 +12,7 @@ class FindTestsController < ApplicationController
   private
 
   def find_hash_data(data_begin, data_end)
+    return if Test.all.empty?
     hash_data = {}
     if data_begin.empty?
       hash_data['data_begin'] = Test.order(testing_date: :asc).first.testing_date
@@ -24,7 +25,6 @@ class FindTestsController < ApplicationController
     else
       hash_data['data_end'] = data_end.to_date
     end
-    puts('1 hash_data',hash_data)
 
     hash_data
   end 
